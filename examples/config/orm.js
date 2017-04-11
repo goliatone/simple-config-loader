9cd29202d5998cbf05c429936ec1791a26b2e376
goliatone <burgosemi@gmail.com>
2017-04-10 21:57:37 -0400

    ...

--- examples/config/orm.js
+++ examples/config/orm.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: '${app.name}',
-    redis:{
+    redis: {
         port: 27012,
         host: process.env.NODE_REDIS_HOST,
         auth: process.env.NODE_REDIS_PORT
