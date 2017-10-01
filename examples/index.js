46fdeea653caabc92601fa0ca6879900b828abff
goliatone <burgosemi@gmail.com>
2017-10-01 13:34:06 -0400

    Update main example

--- examples/index.js
+++ examples/index.js
@@ -1,21 +1,28 @@
 "use strict";
 
-var config = require('..')({
+let config = require('..')({
     basepath: __dirname,
     packagePath: '../package'
 });
 
-var handler = {
-  get: function (receiver, name) {
-      console.log('name', name);
-    return receiver.data[name];
-  }
+let handler = {
+    get: function (receiver, name) {
+        console.log('name', name);
+        return receiver.data[name];
+    }
 };
 
-var c = new Proxy(config, handler);
+let c = new Proxy(config, handler);
 
-//remove noise...
-delete config.data.package;
+/*
+ * "development" by default. You can set
+ * value in options or when calling node:
+ *
+ * `NODE_ENV=production node index.js`
+ */
+console.log('App running environment: %s', c.environment);
 
+console.log(JSON.stringify(c.amqp, null, 4));
+console.log(JSON.stringify(c.cli, null, 4));
 console.log(JSON.stringify(c.orm.redis.port, null, 4));
 console.log(JSON.stringify(c.app.redisConfig, null, 4));
